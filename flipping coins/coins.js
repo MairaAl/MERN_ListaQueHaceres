d3af30956f3897524523f12fcb8a20633f91100a
Maira Al <122545402+MairaAl@users.noreply.github.com>
2024-03-07 09:57:27 -0300

    Added coins file

---
+++ flipping coins/coins.js
@@ -0,0 +1,30 @@
+function tossCoin() {
+  return Math.random() > 0.5 ? "heads" : "tails";
+}
+
+function fiveHeads() {
+  return new Promise((resolve, reject) => {
+    let headsCount = 0;
+    let attempts = 0;
+    while (headsCount < 5) {
+      attempts++;
+      let result = tossCoin();
+      console.log(`${result} was flipped (${attempts})`);
+      if (result === "heads") {
+        headsCount++;
+      } else {
+        headsCount = 0;
+      }
+      if (attempts == 100) {
+        return reject(`Sorry, ${attempts} over 10 times.`);
+      }
+    }
+    return resolve`It took ${attempts} tries to flip five "heads"`;
+  });
+}
+fiveHeads()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+console.log("When does this run now?");
+
+console.log("This is run after the fiveHeadsSync function completes");
